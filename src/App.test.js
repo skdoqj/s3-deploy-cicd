13e1db47fa149d1e211daf2e938d7cde240425ca
suwan <skdoqj@naver.com>
2023-02-28 20:11:36 +0900

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('App reder test', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByRole("heading")
+  
+  expect(heading).toBeInTheDocument();
+  expect(heading.textContent).toBe("Hi! AWS CLI");
 });
